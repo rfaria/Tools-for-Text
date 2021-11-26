bb8daf2c68200cba2fa02d86c8a95f1a5ff5e014
Rodrigo Candido Faria <8796054+rfaria@users.noreply.github.com>
2021-11-25 23:04:19 -0300

    Adding README and boilerplate

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tools for Text
+
+"Tools for Text" is a Python project meant to facilitate some text processing activities.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("olá")
+#!/usr/bin/env python3
+"""
+Tools for Text main.py
+"""
+
+__author__ = "Rodrigo Candido Faria"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
